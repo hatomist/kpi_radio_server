93fb288b2a78233c87965467d04adcdd795d199a
Homeless Atomist <hatomist@prok.pw>
2019-08-05 18:47:35 +0300

    Added wrappers "check_ban" and "check_admin_level" for TelegramUser f-s
    ...and ForbiddenError with BannedError

--- telegram_auth.py
+++ telegram_auth.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass, field
 from typing import Dict, Union
 import pymongo
+from functools import wraps
+from datetime import datetime
 nullable_str = Union[str, None]
 
 
@@ -29,13 +31,68 @@ class TelegramAuth:
         self.__users = self.__mongodb['users']
 
     def get_user(self, user_id: int):
-        got_user = self.__users.find_one({'_id': user_id})
-        if got_user is None:
+        user = self.__users.find_one({'_id': user_id})
+        if user is None:
             return TelegramUser(user_id)
         else:
-            return TelegramUser(**got_user)
+            return TelegramUser(**user)
 
     def update_user(self, user: TelegramUser):
         # because it is NOT A PROTECTED MEMBER!!1!!1!! just uid.
         # noinspection PyProtectedMember
         self.__users.replace_one(filter={'_id': user._id}, replacement=user.__dict__, upsert=True)
+
+
+class ForbiddenError(BaseException):
+    def __init__(self, arg='Insufficient permission level'):
+        self.message = arg
+
+    def __str__(self):
+        return self.message
+
+
+class BannedError(BaseException):
+    def __init__(self, user: TelegramUser = None):
+        self.message = 'This user is banned to do this action'
+        if user is not None:
+            # cause I'll tell it once more - THIS IS NOT A PROTECTED MEMBER!!11!!!1!1
+            # noinspection PyProtectedMember
+            self.message = f'User {user.first_name or ""} {user.second_name or ""} with id {user._id}'\
+                           ' is banned to do this action'
+
+    def __str__(self):
+        return self.message
+
+
+def timestamp():
+    return int(datetime.utcnow().timestamp())
+
+
+def check_admin_level(admin_level: AdminLevel):
+    def level_checker(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                user: TelegramUser = kwargs['user']
+            except KeyError:
+                user: TelegramUser = tuple(filter(lambda x: isinstance(x, TelegramUser), args))[0]
+            if user.admin_level < admin_level:
+                raise ForbiddenError
+            else:
+                return func(*args, **kwargs)
+        return wrapper
+    return level_checker
+
+
+def check_ban(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            user: TelegramUser = kwargs['user']
+        except KeyError:
+            user: TelegramUser = tuple(filter(lambda x: isinstance(x, TelegramUser), args))[0]
+        if timestamp() < user.ban_length + user.ban_timestamp:
+            raise BannedError(user)
+        else:
+            return func(*args, **kwargs)
+    return wrapper
