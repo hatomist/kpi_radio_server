72a87eb27c8c5997da45531758284a1b0ffeafc3
Homeless Atomist <homelessatomist@gmail.com>
2019-08-08 16:49:48 +0300

    Fixed typos and bugs in radio_schedule.py

--- radio_schedule.py
+++ radio_schedule.py
@@ -11,6 +11,7 @@ class RadioSchedule:
         self.__schedule: Dict[str, Dict[str, Dict[str, Union[str, int]]]] = radio_schedule
         self.__cease_continuous_run = None
         self.__radio = radio
+        self.set_schedule(self.__schedule)
 
     @staticmethod
     def __run_scheduler_continuously(interval: int = 1):
@@ -30,12 +31,13 @@ class RadioSchedule:
     def set_schedule(self, radio_schedule: Dict[str, Dict[str, Dict[str, Union[str, int]]]]):
         self.__schedule = radio_schedule
         for day in ('monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'):
-            for at_time in self.__schedule.keys():
-                do_job_scheduled = getattr(schedule.every(), day[0]).at(at_time).do
+            for at_time in self.__schedule[day].keys():
+                do_job_scheduled = getattr(schedule.every(), day).at(at_time).do
                 job = (self.__radio.set_radio_mode, self.__radio.set_auto_mode, None,
                        self.__radio.set_announcement_mode,
                        self.__radio.set_off_mode)[self.__schedule[day][at_time]['radio_mode']]
-                do_job_scheduled(lambda: job(self.__schedule[day][at_time]['air_time']))
+                do_job_scheduled(lambda x_day=day, x_at_time=at_time:
+                                 job(self.__schedule[x_day][x_at_time]['air_time']))
 
     def start(self):
         self.__cease_continuous_run = self.__run_scheduler_continuously()
